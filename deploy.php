c690238ea2de84c0686149321cd80799b8641a09
adler3d <adler3d@gmail.com>
2020-07-24 03:53:11 +0300

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -7,12 +7,18 @@
   }
   function f($fn,$del){return count(explode($del,$fn));}
   header("content-type: text/plain");
+  $data=$_POST['data'];
+  $check=function($k){return array_key_exists($k,$_POST);};
+  $arr=explode(",",'xhr_post,xhr_post.js,from_js,from_node,from_nodejs,qs,querystring,utf8_decode');
+  if(count(array_filter($arr,$check))>0){
+    $data=utf8_decode($data);
+  }
   if(array_key_exists('data',$_POST)){
     $fn=array_key_exists('fn',$_POST)?$_POST['fn']:"g_obj.json";
     //if(f($fn,"/")>1||f($fn,"\\")>1||f($fn,".")>2||f($fn,".json")<1){
     //  echo "wrong fn, but ok, let's use default(g_obj.json)\n";$fn="g_obj.json";
     //}
-    echo file_put_contents(dirname(__FILE__)."/".$fn,$_POST['data']);echo "\n";
+    echo file_put_contents(dirname(__FILE__)."/".$fn,$data);echo "\n";
     echo file_put_contents(dirname(__FILE__)."/last_backup_fn.txt",$fn);echo "\n";
     echo "done!\n";
   }
